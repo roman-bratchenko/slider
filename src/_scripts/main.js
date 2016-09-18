97e792d4f0d9a8bf8de8dabc685d29b2a5859d37
Roman Bratchenko <roman.bratchenko@gmail.com>
2016-09-18 20:47:28 +0300

    load and activate first image

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -1,6 +1,3 @@
-// Main javascript entry point
-// Should handle bootstrapping/starting application
-
 'use strict';
 
 var Gallery = function(domObject) {
@@ -26,6 +23,31 @@ var Gallery = function(domObject) {
     // set initial values
     self.currentImageIndex = 0;
     self.$counterTotal.innerHTML = self.imageCount;
+
+    // load first (current) image
+    self._loadImage(self.currentImageIndex);
+    self._activateImage(self.currentImageIndex);
 };
+
+Gallery.prototype._loadImage = function (index) {
+    var $image = this.$images[index];
+    if ($image.classList.value.indexOf('loaded') < 0) {
+        var self = this;
+        $image.onload = function(event){
+            $image.classList.add('loaded');
+        }
+        $image.setAttribute('src', $image.getAttribute('data-src'));
+    }
+};
+
+Gallery.prototype._activateImage = function(index) {
+    [].forEach.call(this.$images, function(el){
+        el.classList.remove('is-shown');
+    });
+    this.$images[index].classList.add('is-shown');
+    this.currentImageIndex = index;
+};
+
+var gallery = new Gallery(document.querySelector('.gallery'));
 
 
